4c889facb89bbf170f093ca04f1baaedeb7e927e
EdLovesGamesAndPuzzles <edwin.ma@live.nl>
2026-09-05 20:15:17 +0200

    Added water ripples to see where the frogs would land + changed homescreen

--- webgame/main.py
+++ webgame/main.py
@@ -9,12 +9,13 @@ import button
 # async is needed for webapp
 async def main():
 
+    DEBUGGING = False
 
     size = width, height = (1280,720)
     background_color = pg.Color("lightblue")# This sets the background color. Potential contenders are turqouise2, skyblue lightblue etc
     lilypad_num = 5 # Amount of lilypad at the starting level
     lilypad_w = int(min(width/lilypad_num - 10, 250))
-    lilypad_h = 30
+    lilypad_h = 70
     frog_w = lilypad_w//2.5
     position = 2 # position of the selector
     lijst = [1]*lilypad_num #Starting position of the frogs
@@ -22,6 +23,10 @@ async def main():
     selected = False #Has the player selected anything. Start of as False
     all_states = [lijst.copy()] #The current level is one
     game_state = "home" #game states are "home", "htp", "playing", "won"
+
+
+    F = 1.4 #F stands for factor, which is the factor by which the indicator width and height are multiplied by the width and height of the lilypad
+    clicked = False
 
 
     #initialize Pygame
@@ -37,41 +42,71 @@ async def main():
     pg.display.update()
 
     #load images
+    screen_rect = pg.Rect(0,0,width, height)
+
     pond_original = pg.image.load("pond.png").convert_alpha()
-    pond = pg.transform.scale(pond_original, (width, height)).convert_alpha()
+    pond = pg.transform.scale(pond_original, (width, height))
     pond_rect = pg.Rect(0,0,width,height)
 
     htp_original = pg.image.load("HTP-screen.png").convert_alpha()
-    htp = pg.transform.scale(htp_original, (width, height)).convert_alpha()
+    htp = pg.transform.scale(htp_original, (width, height))
     htp_rect = pg.Rect(0,0,width,height)
 
+    homescreen_original = pg.image.load("Homescreen.png").convert_alpha()
+    homescreen = pg.transform.scale(homescreen_original, (width, height))
+    homescreen_rect = pg.Rect(0,0,width,height)
+
+    title_original = pg.image.load("name_of_game.png").convert_alpha()
+    title = pg.transform.scale(title_original, (250,200))
+    title_rect = pg.Rect(500,0, 250,200)
+
     frog_original = pg.image.load("frog.png").convert_alpha()
-    frog = pg.transform.scale(frog_original, (frog_w,frog_w)).convert_alpha()
+    frog = pg.transform.scale(frog_original, (frog_w,frog_w))
     frog_rect = pg.Rect(0,0,frog_w,frog_w)
 
     Lilypad_original = pg.image.load("lilypad.png").convert_alpha()
-    lilypad = pg.transform.scale(Lilypad_original, (lilypad_w, 30)).convert_alpha()
-    lilypad_rect = pg.Rect(0,0,lilypad_w, 30)
+    lilypad = pg.transform.scale(Lilypad_original, (lilypad_w, lilypad_h))
+    lilypad_rect = pg.Rect(0,0,lilypad_w, lilypad_h)
+
+    indicator_original = pg.image.load("landing_indicator2.png").convert_alpha()
+
+    visible_rect = indicator_original.get_bounding_rect()
+    indicator_original = indicator_original.subsurface(visible_rect).copy()
+    indicator = pg.transform.scale(indicator_original, (int(lilypad_w *F), int(lilypad_h*F)))
+    indicator_rect = pg.Rect(0,0,int(lilypad_w*F), int(lilypad_h*F))
+
 
     arrow_original = pg.image.load("arrow.png").convert_alpha()
-    arrow = pg.transform.scale(arrow_original, (100, 100)).convert_alpha()
+    arrow = pg.transform.scale(arrow_original, (100, 100))
     arrow_loc = arrow.get_rect()
     arrow_loc.center = 0.5*width/lilypad_num, height*0.2
 
 
     #Now for the buttons
-    play_img_original = pg.image.load("play_button.png").convert_alpha()
-    htp_img_original = pg.image.load("How_to_play_button.png").convert_alpha()
+    # play_img_original = pg.image.load("play_button.png").convert_alpha()
+    # htp_img_original = pg.image.load("How_to_play_button.png").convert_alpha()
+    # levels_img_original = pg.image.load("levels_button.png").convert_alpha()
+    # credits_img_original = pg.image.load("credits_button.png").convert_alpha()
 
-    play_button = button.Button(100,200, play_img_original, 0.1)
-    htp_button = button.Button(100,400, htp_img_original, 0.1)
+    # play_button = button.Button(100,200, play_img_original, 0.1)
+    # htp_button = button.Button(100,400, htp_img_original, 0.1)
+    # levels_button = button.Button(600, 200, levels_img_original, 0.1)
+    # credits_button = button.Button(600, 400, credits_img_original, 0.1)
+
+
+    play_rect = pg.Rect(472, 332, 343, 90)
+    htp_rect = pg.Rect(472, 447, 343, 77)
+    levels_rect = pg.Rect(472,554, 343, 77)
+
 
 
     #Main loop
     running = True
     while running:
         clock.tick(60)
-        
+
+
+        pos = pg.mouse.get_pos()
         for event in pg.event.get():
                 if event.type == pg.QUIT:
                     running = False
@@ -134,8 +169,9 @@ async def main():
                             game_state = "home"
                         
 
+
          
-                            
+          
 
                         
 
@@ -145,15 +181,43 @@ async def main():
         
 
         if game_state == "home":
-            screen.blit(pond,pond_rect)
-            if play_button.draw(screen) ==True:
-                game_state = "playing"
-            if htp_button.draw(screen) == True:
-                game_state = "htp"
+            
+            #check mouseover and clicked conditions
+            if play_rect.collidepoint(pos):
+                if pg.mouse.get_pressed()[0] == 1 and clicked == False:
+                    clicked = True
+                    game_state = "playing"
+
+
+            if htp_rect.collidepoint(pos):
+                if pg.mouse.get_pressed()[0] == 1 and clicked == False:
+                    clicked = True
+                    game_state = "htp"
+
+            if pg.mouse.get_pressed()[0] == 0:
+                clicked = False
+
+            screen.blit(homescreen, homescreen_rect)
+
+            # pg.draw.rect(screen, "red", play_rect, 3)
+            # pg.draw.rect(screen, "red", htp_rect, 3)
+            # pg.draw.rect(screen, "red", levels_rect, 3)
+
+
+
+            # screen.blit(pond,pond_rect)
+            # screen.blit(title,title_rect)
+            # if play_button.draw(screen) ==True:
+            #     game_state = "playing"
+            # if htp_button.draw(screen) == True:
+            #     game_state = "htp"
+            # levels_button.draw(screen)
+            # credits_button.draw(screen)
+            
 
 
         if game_state == 'htp':
-            screen.blit(htp, htp_rect)
+            screen.blit(htp, screen_rect)
 
         
         if game_state == "playing":
@@ -161,9 +225,25 @@ async def main():
             #Fill background
             screen.blit(pond, pond_rect)
             #Draw lilypads
+            if selected == True:
+                indicator_left = position -(2*lijst[position]-1)
+                indicator_right = position +(2*lijst[position]-1)
+                if indicator_left >= 0 and lijst[indicator_left]>0:
+                    indicator_rect.center = ((indicator_left + 0.5) * width//lilypad_num, int(0.8*height))
+                    screen.blit(indicator, indicator_rect)
+                if indicator_right <len(lijst) and lijst[indicator_right]>0:
+                    indicator_rect.center = ((indicator_right + 0.5)*width//lilypad_num, int(0.8*height))
+                    screen.blit(indicator,indicator_rect)
+
             for i in range(0,lilypad_num): 
-                lilypad_rect.center = ((i+0.5)*width/lilypad_num, int(0.8*height))
-                screen.blit(lilypad, lilypad_rect)
+                            lilypad_rect.center = ((i+0.5)*width//lilypad_num, int(0.8*height))
+                            screen.blit(lilypad, lilypad_rect)
+
+                            if DEBUGGING == True:
+                                 indicator_rect.center = lilypad_rect.center
+                                 screen.blit(indicator,indicator_rect)
+
+
 
             #Draw arrow above
             arrow_rect.center = ((position+0.5)*width/lilypad_num, 0.3*height+selected*0.1*height)
@@ -180,13 +260,20 @@ async def main():
                 lijst = [1]*lilypad_num
                 all_states = [lijst.copy()]
                 lilypad_w = int(min(width/lilypad_num - 10, 200))
-                lilypad_rect = pg.Rect(0,0,lilypad_w, 30)
-                lilypad = pg.transform.scale(Lilypad_original, (lilypad_w, 30)).convert_alpha()
+                lilypad_rect = pg.Rect(0,0,lilypad_w, lilypad_h)
+                lilypad = pg.transform.scale(Lilypad_original, (lilypad_w, lilypad_h))
+                indicator = pg.transform.scale(indicator_original, (int(lilypad_w*F), int(lilypad_h*F)))
+                indicator_rect = pg.Rect(0,0,int(lilypad_w*F), int(lilypad_h*F))
                 frog_w = lilypad_w//2.5
                 game_state = "won"
                 screen.blit(text_surface, (0,0))
 
+        if pg.mouse.get_pressed()[0] == 1 and clicked == False:
+                    clicked = True
+                    print(pos)
 
+        if pg.mouse.get_pressed()[0] == 0:
+            clicked = False      
 
             
     
@@ -201,5 +288,3 @@ async def main():
     pg.quit()
 
 asyncio.run(main())
-
-#########################
